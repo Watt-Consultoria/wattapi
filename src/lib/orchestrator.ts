6d484ec9bc1e46762690596fc46cb7b77c13f527
yanlima <yangclima@gmail.com>
2026-05-26 09:56:47 -0300

    test: remove `orchestrator.waitForWebServer` logs

--- src/lib/orchestrator.ts
+++ src/lib/orchestrator.ts
@@ -25,21 +25,17 @@ async function waitForAllServices() {
   await waitForWebServer();
 
   async function waitForWebServer() {
-    process.stdout.write('Waiting for web server to be ready');
-
     await retry(fetchStatusPage, {
       retries: 100,
       maxTimeout: 1000,
     });
 
     async function fetchStatusPage() {
-      process.stdout.write('.');
       const response = await fetch('http://localhost:3000/status');
 
       if (response.status !== 200) {
         throw Error();
       }
-      process.stdout.write(' Web server is ready!\n');
     }
   }
 }
